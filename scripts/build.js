95db6e06711ead80a09ec8dbec44790d9482e09c
Zamith <zamith.28@gmail.com>
2018-04-06 18:33:40 +0100

    Adds docs

--- scripts/build.js
+++ scripts/build.js
@@ -5,15 +5,15 @@ var Path = require('path');
 require('shelljs/global');
 set('-e');
 
-mkdir('-p', 'web_deploy')
+mkdir('-p', 'docs')
 
-cp('-R', 'web/*', 'web_deploy/');
+cp('-R', 'web/*', 'docs/');
 
-exec('npm run swagger bundle --        -o web_deploy/swagger.json');
-exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
+exec('npm run swagger bundle --        -o docs/swagger.json');
+exec('npm run swagger bundle -- --yaml -o docs/swagger.yaml');
 
 var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
-rm('-rf', 'web_deploy/swagger-ui/')
-cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
-sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
+rm('-rf', 'docs/swagger-ui/')
+cp('-R', SWAGGER_UI_DIST, 'docs/swagger-ui/')
+sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'docs/swagger-ui/index.html')
 
