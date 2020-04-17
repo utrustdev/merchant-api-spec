c429fe4a21953ec4b52c4554acc8d1877d69ad75
Daniel Coelho <danielmartinscoelho@gmail.com>
2020-04-17 15:53:21 +0100

    Add JavaScript create order code sample

---
+++ spec/code_samples/JavaScript/stores@orders/post.js
@@ -0,0 +1,50 @@
+// configs
+  const API_ROOT = "https://merchants.api.utrust.com/api/stores/orders" // OR "https://merchants.api.sandbox-utrust.com/api/stores/orders" (for sandbox)
+  const API_KEY = // Add your API_KEY here OR fetch it from environment variables;
+
+  // order params
+  const orderParams = {
+      data: {
+          type: "orders",
+          attributes: {
+              order: {
+                  reference: "order-51367",
+                  amount: {
+                      total: "10.00",
+                      currency: "EUR",
+                  },
+                  return_urls: {
+                      return_url: "http://example.com/success",
+                  },
+                  line_items: [{
+                      sku: "item-unique-id",
+                      name: "Donation",
+                      price: "10.00",
+                      currency: "EUR",
+                      quantity: 1
+                  }]
+              },
+              customer: {
+                  email: 'customer@example.com',
+                  country: 'US'
+              },
+          },
+      },
+  }
+
+  // utrust api
+  const utrustApi = {
+      createOrder: (params, token) =>
+          fetch(API_ROOT + "/stores/orders/", {
+              method: "POST",
+              headers: {
+                  "content-type": "application/vnd.api+json",
+                  authorization: "Bearer " + API_KEY,
+              },
+              body: JSON.stringify(params),
+          }).then(response => response.json()),
+  }
+
+  utrustApi
+  .createOrder(orderParams)
+  .then(response => response.data.attributes.redirect_url)
